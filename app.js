0aa9d4b4cca36904e460c38aa723e0b085545f0f
Madeline Wong <madelinegwong@outlook.com>
2018-01-31 20:31:36 -0500

    first box toggles red!

---
+++ app.js
@@ -0,0 +1,16 @@
+//variables
+const resetBtn = document.querySelector('#reset');
+const box = document.querySelector('.box');
+
+//eventListeners 
+box.addEventListener('click', toggleRed);
+resetBtn.addEventListener('click', clearGrid);
+//fuctions
+function toggleRed(e){
+    const element = e.target;
+    element.classList.toggle('red');
+}
+
+function clearGrid(e){
+    console.log('clearing the grid');
+}
