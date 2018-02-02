9cb0a9b244c335b71ba6411d2563eeebb5c348e9
Madeline Wong <madelinegwong@outlook.com>
2018-02-02 18:14:48 -0500

    changed the color picker so it's able to change and store colors, and made it functioning.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //variables
 const resetBtn = document.querySelector('#reset');
-const colorPicker = document.querySelector('.colorPicker');
+const picker = document.querySelector('#picker');
+// const box = document.querySelector('.box');
 const gridSizeForm = document.querySelector('#grid-size');
 const main = document.querySelector('main');
 
@@ -12,12 +13,12 @@ gridSizeForm.addEventListener('submit', changeGrid);
 changeGrid();
 
 //functions
-function toggleRed(e){
-    const element = e.target;
-    element.classList.
-    
-}
 
+
+function changeBoxColor(e){
+    const element = e.target; 
+    element.style.backgroundColor = picker.value;
+}
 
 function clearGrid(e){
     const boxes = document.querySelectorAll('.box');
@@ -26,40 +27,40 @@ function clearGrid(e){
     });
 }
 
+
 function changeGrid(e){
     if(e) {
-    e.preventDefault();
+        e.preventDefault();
     }
     const gridSizeValue = +gridSizeForm.querySelector('input:checked').value;
     const grid = makeGrid(gridSizeValue);
-//set the main to the grid 
+    //set the main to the grid 
    main.innerHTML = ''; //this clears the main area
    grid.forEach((row) => {
      main.appendChild(row);  
-   })
+   });
 
 }
 
 function makeGrid(size){  
     const rows = [];
     for(let i = 0; i < size; i += 1){
-    //make the grid
-    //make a div with class of row 
-    const row = document.createElement('div');
-    row.classList.add('row');
-    //make a div with class of col-md-12 with a class of row 
-    const column = document.createElement('div'); 
-    column.classList.add('col-md-12');
-    row.appendChild(column); 
-    //make six divs with class of box inside of col-md-12
-    for(let i = 0; i < size; i += 1){
-        const box = document.createElement('div'); 
-        box.classList.add('box'); 
-        box.addEventListener('click', toggleRed);
-        column.appendChild(box);
+        //make the grid
+        //make a div with class of row 
+        const row = document.createElement('div');
+        row.classList.add('row');
+        //make a div with class of col-md-12 with a class of row 
+        const column = document.createElement('div'); 
+        column.classList.add('col-md-12');
+        row.appendChild(column); 
+        //make six divs with class of box inside of col-md-12
+        for(let i = 0; i < size; i += 1){
+            const box = document.createElement('div'); 
+            box.classList.add('box'); 
+            box.addEventListener('click', changeBoxColor);
+            column.appendChild(box);
         }
-        rows.push(row);
-
+    rows.push(row);
     }
     return rows;
 }
